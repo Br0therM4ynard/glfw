15d14dff3845291ddfa5dd3183993fc9bd4a4473
Christopher Stevens <obi_wan02@hotmail.com>
2020-05-27 19:57:27 +0100

    Removed unnecessary file includes for glfw

--- premake5.lua
+++ premake5.lua
@@ -27,21 +27,15 @@ project "GLFW"
 
         files
         {
-            "src/cocoa_platform.h",
-            "src/cocoa_joystick.h",
-            "src/posix_thread.h",
-            "src/nsgl_context.h",
-            "src/egl_context.h",
-            "src/osmesa_context.h",
             "src/cocoa_init.m",
             "src/cocoa_joystick.m",
             "src/cocoa_monitor.m",
-            "src/cocoa_window.m",
             "src/cocoa_time.c",
             "src/posix_thread.c",
-            "src/nsgl_context.m",
+            "src/cocoa_window.m",
             "src/egl_context.c",
-            "src/osmesa_context.c",
+            "src/nsgl_context.m",
+            "src/osmesa_context.c"
         }
 
     filter "system:linux"
